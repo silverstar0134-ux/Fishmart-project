ed64e2f4b06dc51a85d0e2ef95059196ce63b898
silverstar8870-sys <silverstar8870@gmail.com>
2026-03-03 19:55:10 +0530

    added static root

--- fish/settings.py
+++ fish/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# Media files (User uploads)
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
